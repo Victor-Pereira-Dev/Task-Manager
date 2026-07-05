aabe94eec74957fe51e9c3b6ec4245e1b05bde1c
Victor-Pereira-Dev <victorflorespereira2005@gmail.com>
2026-07-05 16:25:47 -0300

    FEAT: Alteração para incluir dropdown como menu de tarefas para futuras opções de navegação.

---
+++ Frontend/index/script.js
@@ -0,0 +1,6 @@
+﻿const botao = document.getElementById('grid-btn');
+const grid = document.getElementById('meu-grid');
+
+botao.addEventListener('click', () => {
+    grid.classList.toggle('ativo');
+});
