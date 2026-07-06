d48591018a1d37aaef09b532ba9ce9f82f0298e4
Victor-Pereira-Dev <victorflorespereira2005@gmail.com>
2026-07-06 12:30:57 -0300

    FEAT: Ajuste no menu colocando as opções que o projeto irá ter e colocando uma cor mais forte.

--- Frontend/index/script.js
+++ Frontend/index/script.js
@@ -1,6 +1,11 @@
 ﻿const botao = document.getElementById('grid-btn');
+const botaoFechar = document.getElementById('grid-btn-fechar');
 const grid = document.getElementById('meu-grid');
 
 botao.addEventListener('click', () => {
     grid.classList.toggle('ativo');
+});
+
+botaoFechar.addEventListener('click', () => {
+    grid.classList.remove('ativo');
 });
